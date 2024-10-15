ad9fab5b43432f2ef80b70466d90ba3585fc804a
Andrey Chasov <andrey.chasov@web-pe.ru>
2024-10-15 22:57:36 +0300

    Edit Draw

--- src/app/app.component.data.ts
+++ src/app/app.component.data.ts
@@ -2,7 +2,7 @@ export const Config: object = {
   headerV: ["HTML", "CSS", "JavaScript"],
   htmlV: ["Верстка", "Компоненты", "Язык Markdown", "Препроцессоры HTML", "Шаблонизаторы"],
   cssV: ["LESS/SASS", "Tailwind CSS", "Bootstrap", "Анимации"],
-  jsV: ["Angular CLI", "AJAX/fetch/HTTP", "WebSocket", "Express JS", "Node JS", "TypeScript", "TR"],
+  jsV: ["Angular CLI", "AJAX/fetch/HTTP", "WebSocket", "Express JS", "Node JS", "TypeScript"],
 
 }
 
